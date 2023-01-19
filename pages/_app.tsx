b41ac3f53084de9fbf7f3b39d83a874f81667941
minhnguyen <minhnguyen@rionlab.com>
2023-01-19 14:34:38 +0700

    font t optimize

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,46 @@
-import '../styles/globals.scss';
+import localFont from "@next/font/local";
+import "../styles/globals.scss";
 // Import css files
-import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-import { EmptyLayoutComponent } from '@/layout/empty';
-import { AppPropsWithLayout } from '@/models/common';
+import { EmptyLayoutComponent } from "@/layout/empty";
+import { AppPropsWithLayout } from "@/models/common";
+
+const SourceHanSerif = localFont({
+  src: [
+    {
+      path: "../assets/fonts/SourceHanSerif-Regular.otf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../assets/fonts/SourceHanSerif-Medium.otf",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../assets/fonts/SourceHanSerif-Bold.otf",
+      weight: "700",
+      style: "normal",
+    },
+  ],
+  fallback: ['Helvetica', 'ui-sans-serif']
+});
 
 export default function App({ Component, pageProps }: AppPropsWithLayout) {
-
   const Layout = Component.Layout ?? EmptyLayoutComponent;
 
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  )
+    <>
+      <style jsx global>{`
+        :root {
+          --font-base: ${SourceHanSerif.style.fontFamily};
+        }
+      `}</style>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
+  );
 }
