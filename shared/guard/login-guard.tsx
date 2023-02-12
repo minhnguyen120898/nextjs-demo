f2bb375584c5e8fea1b75a096fe94a942f4a6dc7
minhnguyen120898 <minhnguyen120898@gmail.com>
2023-02-12 20:33:48 +0700

    fix build

--- shared/guard/login-guard.tsx
+++ shared/guard/login-guard.tsx
@@ -8,32 +8,20 @@ export interface LoginGuardProps {
   children: ReactNode
 }
 
-<<<<<<< Updated upstream
-export function App ({children}: LoginGuardProps) {
-=======
 // This component is used to protect routes or pages 
 // from unauthorized access by checking the user's authentication status.
 export function LoginGuard ({children}: LoginGuardProps) {
->>>>>>> Stashed changes
   const router = useRouter();
   const { profile, firstLoading } = useAuth();
 
   useEffect(() => {
-<<<<<<< Updated upstream
-    if (!firstLoading && !profile?.id) {
-=======
     // indicating the user is not logged in
     if (!firstLoading && !profile) {
->>>>>>> Stashed changes
       router.push('/login');
     }
   }, [router, profile, firstLoading])
 
-<<<<<<< Updated upstream
-  if (!profile?.id) {
-=======
   if (!profile) {
->>>>>>> Stashed changes
     return <LoadingComponent />
   }
 
